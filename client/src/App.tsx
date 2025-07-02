5b60820283c73e3b1274638846ea965a0953db86
shubham-Alhat <shubhamalhat520@gmail.com>
2025-07-02 22:37:44 +0530

    setup frontend and  backend successfully

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-600 text-5xl">hellow world</div>
+      <Dialog>
+        <DialogTrigger>
+          <Button>Open Dialog</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
+
+export default App;
